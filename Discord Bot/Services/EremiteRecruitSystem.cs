6661b5e0bca93e85553e797d818b39a30c3969e1
Denys Bondarenko <denys.bondarenko@nure.ua>
2022-11-12 06:52:57 +0200

    Eremite after enroll also sends link to info about

--- Discord Bot/Services/EremiteRecruitSystem.cs
+++ Discord Bot/Services/EremiteRecruitSystem.cs
@@ -13,6 +13,7 @@ namespace DiscordBot.Services
     {
         public const string JSON_MERCENARIES_FOLDER = "eremites_recruit_system";
         public const string JSON_MERCENARIES_DATABASE = "eremites_recruits.json";
+        public const string DOCUMENTATION_URL = "https://docs.google.com/document/d/1kO8hHnboGeMsSsdFOT-LwKPUa2rxmlWIGya-gj65amg/edit#heading=h.b9o9wrlffcje";
 
         public List<EremiteRecruit> recruitsCached = new List<EremiteRecruit>();
 
@@ -44,9 +45,10 @@ namespace DiscordBot.Services
                     if (recruit.uid != uid) continue;
 
                     var builderError = new DiscordMessageBuilder();
-                    //var buttonLink = new DiscordLinkButtonComponent();
-                    //builderError.AddComponents()
+                    var buttonLink = new DiscordLinkButtonComponent(DOCUMENTATION_URL, "More about Eremites Recruit System");
+
                     builderError.WithContent($"You already enrolled for Eremites Recruit System this week with UID: {uid}\nThanks for helping with our commisions across Sumeru!");
+                    builderError.AddComponents(buttonLink);
                     await ctx.Member.SendMessageAsync(builderError);
                     return;
                 }
@@ -58,7 +60,10 @@ namespace DiscordBot.Services
             await File.WriteAllTextAsync(fullPath, JsonConvert.SerializeObject(recruitsCached));
 
             var builderSuccess = new DiscordMessageBuilder();
+            var buttonAbout = new DiscordLinkButtonComponent(DOCUMENTATION_URL, "More about Eremites Recruit System");
+
             builderSuccess.WithContent($"You have successfully enrolled for Eremites Recruit System this week with UID: {uid}\nThanks for helping with our commisions! Travel across Sumeru to obtain rare rewards.");
+            builderSuccess.AddComponents(buttonAbout);
             await ctx.Member.SendMessageAsync(builderSuccess);
         }
     }
